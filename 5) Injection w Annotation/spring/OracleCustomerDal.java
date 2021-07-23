e2af276014d0306029417df293b73069eae86e37
codesigned4 <erkanbinay25@gmail.com>
2021-07-24 02:16:54 +0300

    Create OracleCustomerDal.java

---
+++ 5) Injection w Annotation/spring/OracleCustomerDal.java
@@ -0,0 +1,11 @@
+package com.spring;
+
+public class OracleCustomerDal implements ICustomerDal{
+
+	@Override
+	public void add() {
+		System.out.println("Veriniz Oracle Veritabanýna eklendi");
+	}
+	
+
+}
