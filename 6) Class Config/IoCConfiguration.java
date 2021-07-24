ce9321e33908eafbd3a630c5be70f8f4c74f33a7
codesigned4 <erkanbinay25@gmail.com>
2021-07-24 21:57:15 +0300

    Create IoCConfiguration.java

---
+++ 6) Class Config/IoCConfiguration.java
@@ -0,0 +1,10 @@
+package springIntro;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("springIntro")
+public class IoCConfiguration {
+	
+}
